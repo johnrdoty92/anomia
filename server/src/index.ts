34f565ef8feefd8ad331bfa5035ca02db25a09da
John Doty <johnrdoty92@gmail.com>
2024-09-14 15:51:05 -0400

    init socket server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,10 @@
+import { ClientToServerEvents, ServerToClientEvents, SocketData } from "anomia-shared";
+import { Server } from "socket.io";
 
+const io = new Server<ClientToServerEvents, ServerToClientEvents, {}, SocketData>();
+
+io.on("connection", (socket) => {
+  // TODO: create functions for each handler
+});
+
+io.listen(parseInt(process.env.PORT));
--- shared/index.ts
+++ shared/index.ts
@@ -1,1 +1,23 @@
-export const message = "hello world"
+export type DeckId = "default";
+
+export type ServerToClientEvents = {
+  gameStarted: () => void;
+  gameCreated: (gameId: string) => void;
+  playerJoined: ({ name }: { name: string }) => void;
+  gameStatus: (gameStatus: unknown) => void;
+  gameOver: () => void;
+};
+
+export type ClientToServerEvents = {
+  createGame: ({ adminName, deckId }: { adminName: string; deckId: DeckId }) => void;
+  joinGame: ({ name, gameId }: { name: string; gameId: string }) => void;
+  takeTurn: ({ playerIndex, gameId }: { playerIndex: number; gameId: string }) => void;
+  claimCard: ({ playerIndex }: { playerIndex: number }) => void;
+  restartGame: ({ gameId }: { gameId: string }) => void;
+  endGame: ({ gameId }: { gameId: string }) => void;
+};
+
+export type SocketData = {
+  name: string;
+  index: number;
+};
