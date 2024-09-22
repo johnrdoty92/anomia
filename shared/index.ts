82ddb6724b752ecead48366d97b3f7c9e6c07221
John Doty <johnrdoty92@gmail.com>
2024-09-22 09:56:19 -0400

    update socket types

--- shared/index.ts
+++ shared/index.ts
@@ -1,23 +1,59 @@
-export type DeckId = "default";
+export const DeckIds = ["default"] as const 
+
+export type DeckId = typeof DeckIds[number] 
+
+export type Response<T> = (
+  response: { success: true, data: T, message?: never } | { success: false, data?: never, message: string }
+) => void
+
+export type PlayerPayload = {
+  gameId: string
+  id: string
+  index: number
+  name: string
+}
+
+export type GameStatePayload = {
+  faceOff: [number, number] | null
+  activeCards: {
+    player: PlayerPayload,
+    card: {
+      index: number
+      id: string;
+      shape1: number;
+      shape2: number | null;
+      deckId: string;
+      topic: string;
+    }
+  }
+}
+
+export type GamePayload = {
+  id: string
+  players: { name: string, id: string }[]
+}
+
 
 export type ServerToClientEvents = {
-  gameStarted: () => void;
-  gameCreated: (gameId: string) => void;
-  playerJoined: ({ name }: { name: string }) => void;
-  gameStatus: (gameStatus: unknown) => void;
-  gameOver: () => void;
+  gameStarted: (gameState: GameStatePayload) => void;
+  playerJoined: ({ name, id }: { name: string, id: string }) => void;
+  gameStatus: (gameState: GameStatePayload) => void;
+  gameOver: (winner: string) => void;
 };
 
 export type ClientToServerEvents = {
-  createGame: ({ adminName, deckId }: { adminName: string; deckId: DeckId }) => void;
-  joinGame: ({ name, gameId }: { name: string; gameId: string }) => void;
-  takeTurn: ({ playerIndex, gameId }: { playerIndex: number; gameId: string }) => void;
-  claimCard: ({ playerIndex }: { playerIndex: number }) => void;
-  restartGame: ({ gameId }: { gameId: string }) => void;
-  endGame: ({ gameId }: { gameId: string }) => void;
+  createGame: ({ adminName, deckId }: { adminName: string; deckId: DeckId }, response: Response<PlayerPayload>) => void;
+  joinGame: ({ name, gameId }: { name: string; gameId: string }, response: Response<{game: GamePayload, player: PlayerPayload}>) => void;
+  startGame: (gameId: string, response: Response<GameStatePayload>) => void
+  takeTurn: ({ playerIndex, gameId }: { playerIndex: number; gameId: string }, response: Response<GameStatePayload>) => void;
+  claimCard: ({ playerIndex }: { playerIndex: number }, response: Response<GameStatePayload>) => void;
+  restartGame: ({ gameId }: { gameId: string }, response: Response<GamePayload>) => void;
+  endGame: ({ gameId }: { gameId: string }, reponse: Response<GameStatePayload>) => void;
 };
 
-export type SocketData = {
+export type PlayerSocketData = Partial<{
+  id: string;
+  gameId: string;
   name: string;
   index: number;
-};
+}>;
